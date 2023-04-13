83f409aa9453a6ff1028633d972377ef9288c950
jovanas00 <jovanastojanovic394@gmail.com>
2023-04-13 12:03:30 +0200

    IzmeniKlijenta, IzmeniSalon

--- Aplikacija/Backend/Controllers/KlijentController.cs
+++ Aplikacija/Backend/Controllers/KlijentController.cs
@@ -10,21 +10,44 @@ public class KlijentController : ControllerBase
         Context = context;
     }
 
-    [Route("IzmeniProfilKlijenta/{id_klijent}")]
+    // [Route("IzmeniProfilKlijenta/{id_klijent}")]
+    // [HttpPut]
+    // public async Task<ActionResult<Klijent>> IzmeniProfil(int id_klijent, Klijent noviKlijent)
+    // {
+    //     var k = await Context.Klijenti.FindAsync(id_klijent);
+    //     if(k == null)
+    //         return NotFound();
+    //     try{
+    //         k.ime = noviKlijent.ime;
+    //         k.prezime = noviKlijent.prezime;
+    //         k.adresa = noviKlijent.adresa;
+    //         k.grad = noviKlijent.grad;
+    //         k.brojTelefona = noviKlijent.brojTelefona;
+    //         await Context.SaveChangesAsync();
+
+    //         return Ok(k);
+    //     }
+    //     catch(Exception e)
+    //     {
+    //         return BadRequest(e.Message);
+    //     }
+    // }
+
+    [Route("IzmeniProfilKlijenta/{korisnicko_ime}/{ime}/{prezime}/{adresa}/{grad}/{brojTelefona}")]
     [HttpPut]
-    public async Task<ActionResult<Klijent>> IzmeniProfil(int id_klijent, Klijent noviKlijent)
+    public async Task<ActionResult<Klijent>> IzmeniProfil(string korisnicko_ime, string ime, string prezime, string adresa, string grad, string brojTelefona)
     {
-        var k = await Context.Klijenti.FindAsync(id_klijent);
+        var k = await Context.Korisnici.Where(p => p.korisnickoIme == korisnicko_ime ).FirstOrDefaultAsync();
         if(k == null)
             return NotFound();
+        var klijent = await Context.Klijenti.Where(p=>p.Korisnik==k).FirstOrDefaultAsync();
         try{
-            k.ime = noviKlijent.ime;
-            k.prezime = noviKlijent.prezime;
-            k.adresa = noviKlijent.adresa;
-            k.grad = noviKlijent.grad;
-            k.brojTelefona = noviKlijent.brojTelefona;
+            klijent.ime = ime;
+            klijent.prezime = prezime;
+            klijent.adresa = adresa;
+            klijent.grad = grad;
+            klijent.brojTelefona = brojTelefona;
             await Context.SaveChangesAsync();
-
             return Ok(k);
         }
         catch(Exception e)
--- Aplikacija/Backend/Controllers/SalonController.cs
+++ Aplikacija/Backend/Controllers/SalonController.cs
@@ -10,22 +10,45 @@ public class SalonController : ControllerBase
         Context = context;
     }
 
-    [Route("IzmeniProfilSalona/{id_salon}")]
-    [HttpPut]
-    public async Task<ActionResult> IzmeniProfil(int id_salon, Salon noviSalon)
-    {
-        var s = await Context.Saloni.FindAsync(id_salon);
-        if(s == null)
-            return NotFound();
-        try{
-            s.naziv = noviSalon.naziv;
-            s.adresa = noviSalon.adresa;
-            s.grad = noviSalon.grad;
-            //s.ProsecnaOcena = noviSalon.ProsecnaOcena; - treba li ovo kod izmenu profila
-            s.brojTelefona = noviSalon.brojTelefona;
-            await Context.SaveChangesAsync();
+    // [Route("IzmeniProfilSalona/{id_salon}")]
+    // [HttpPut]
+    // public async Task<ActionResult> IzmeniProfil(int id_salon, Salon noviSalon)
+    // {
+    //     var s = await Context.Saloni.FindAsync(id_salon);
+    //     if(s == null)
+    //         return NotFound();
+    //     try{
+    //         s.naziv = noviSalon.naziv;
+    //         s.adresa = noviSalon.adresa;
+    //         s.grad = noviSalon.grad;
+    //         //s.ProsecnaOcena = noviSalon.ProsecnaOcena; - treba li ovo kod izmenu profila
+    //         s.brojTelefona = noviSalon.brojTelefona;
+    //         await Context.SaveChangesAsync();
 
-            return Ok();
+    //         return Ok();
+    //     }
+    //     catch(Exception e)
+    //     {
+    //         return BadRequest(e.Message);
+    //     }
+    // }
+
+    [Route("IzmeniProfilSalona/{korisnicko_ime}/{naziv}/{adresa}/{grad}/{brojTelefona}")]
+    [HttpPut]
+    public async Task<ActionResult<Salon>> IzmeniProfil(string korisnicko_ime, string naziv, string adresa, string grad, string brojTelefona)
+    {
+        var k = await Context.Korisnici.Where(p => p.korisnickoIme == korisnicko_ime ).FirstOrDefaultAsync();
+        if(k == null)
+            return NotFound();
+        var s = await Context.Saloni.Where(p=>p.Korisnik==k).FirstOrDefaultAsync();
+        try{
+            s.naziv = naziv;
+            s.adresa = adresa;
+            s.grad = grad;
+            //s.ProsecnaOcena = noviSalon.ProsecnaOcena; - treba li ovo kod izmenu profila
+            s.brojTelefona = brojTelefona;
+            await Context.SaveChangesAsync();
+            return Ok(s);
         }
         catch(Exception e)
         {
