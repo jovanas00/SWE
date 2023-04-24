81624753dd36ed3cdb12df28af1a954d3f0ef75b
jovanas00 <jovanastojanovic394@gmail.com>
2023-04-24 19:25:31 +0200

    Pocetna.js

---
+++ Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Pocetna.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Slider from './Slider';
+import Footer from './Footer';
+
+const Pocetna = () => {
+    return(
+        <Fragment>
+            <Header/>
+            <Slider/>
+            <Footer/>
+        </Fragment>
+    );
+}
+
+export default Pocetna;
