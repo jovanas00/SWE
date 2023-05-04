fee8b8a28aa1ee5d36d0fe6a31e8b808b255b08f
jovanas00 <jovanastojanovic394@gmail.com>
2023-05-04 15:10:10 +0200

    SalonStranica

---
+++ Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonStranica.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Header from '../Pocetna/Header';
+import SalonCard from './SalonCard';
+import Card from '../UI/Card';
+import SalonInfo from './SalonInfo';
+import "./SalonStranica.css";
+
+const SalonStranica = () => {
+    const { id } = useParams(); // uzima se id iz Route(prosledjuje se u App.js)
+    console.log(id);
+    const [salon, setSalon] = useState(null); // inicijalizuje se state salon na null
+
+    useEffect(() => {
+        // uzima se salon iz baze
+        axios.get(`http://localhost:5079/Salon/VratiSalon/${id}`)
+            .then(response => {
+                console.log(response.data);
+                setSalon(response.data); // setujemo novi state salon na dobijene podatke
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, [id]); // ova se funkcija zove kad se promeni id
+
+    if (!salon) {
+        return <div>Loading...</div>; // iz nekog razloga kad ova rpovera ne postoji greska se javi 
+    }//ovde se samo treba pojavi ova poruka dok se ne uzmu podaci iz baze
+
+    return (
+        <div>
+            <Header />
+            
+            <div className="salon-info"> 
+                <div className="salon-card__div">
+                    <SalonCard salon={salon} />
+                </div>
+                {/* ako se ne koristi SalonCard */}
+                {/* <div className="salon-card__image">
+                    <img src={icon} alt="{salon.naziv}" />
+                </div> */}
+                {/* <div className="salon-card__text">
+                    <h5>Naziv: {salon.naziv}</h5>
+                    <p>Adresa: {salon.adresa}</p>
+                    <p>Grad: {salon.grad}</p>
+                    <p>Broj telefona: {salon.brojTelefona}</p>
+                    <p>Prosecna ocena: {salon.prosecnaOcena}</p>
+                </div> */}
+                <div className="salon-info__items">
+                    <SalonInfo salon={salon} id={id}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SalonStranica;
