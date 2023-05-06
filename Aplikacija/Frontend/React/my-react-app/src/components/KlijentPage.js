215cdbf40bffef75762913417a8557ab37f91c75
AleksaT <95313323+Aleksa205@users.noreply.github.com>
2023-05-06 20:13:42 +0200

    KlijentPage.js test

---
+++ Aplikacija/Frontend/React/my-react-app/src/components/KlijentPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const KlijentPage = () => {
+  return (
+    <div>
+      <h2>Dobrodošli na stranicu Klijenta!</h2>
+      <p>Ovde možete pregledati informacije o klijentu i obavljati odgovarajuće akcije.</p>
+      {/* Dodajte odgovarajući sadržaj za stranicu Klijenta */}
+    </div>
+  );
+};
+
+export default KlijentPage;
