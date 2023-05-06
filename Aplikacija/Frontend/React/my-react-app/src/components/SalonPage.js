cfe032eca72c3343c5bd2f9e8b365324b77d5ba1
AleksaT <95313323+Aleksa205@users.noreply.github.com>
2023-05-06 20:15:15 +0200

    SalonPage.js test

---
+++ Aplikacija/Frontend/React/my-react-app/src/components/SalonPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SalonPage = () => {
+  return (
+    <div>
+      <h2>Dobrodošli na stranicu Salona!</h2>
+      <p>Ovde možete pregledati informacije o salonu i njegove usluge.</p>
+      {/* Dodajte odgovarajući sadržaj za stranicu Salona */}
+    </div>
+  );
+};
+
+export default SalonPage;
