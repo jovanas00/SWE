129e2c351926f2a8b7856d4747e47de9c60cfb48
unknown <danilonikolic@elfak.rs>
2023-05-09 19:19:48 +0200

    Dodavanje u korpu

--- Aplikacija/Frontend/React/my-react-app/src/components/Saloni/Proizvodi.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Saloni/Proizvodi.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../UI/Card";
 import './Proizvodi.css';
+import { vratiRole } from "../Auth/VratiRole";
+import Cookies from "js-cookie"
+import { BsCart } from 'react-icons/bs';
 
 const Proizvodi = ({ id }) => {
     const [proizvodi, setProizvodi] = useState([]);
+    const [proizvodID, setProizvodID] = useState();
     const [kategorije, setKategorije] = useState([]);
 
     useEffect(() => {
@@ -16,6 +20,28 @@ const Proizvodi = ({ id }) => {
                 console.log(error);
             });
     }, [id]);
+
+    const token = Cookies.get('token');
+    const config = {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    const handleDodajKorpa =
+        async (proizvodID) => {
+            try {
+                const response = await axios.put(`http://localhost:5169/Klijent/DodajUKorpu/${proizvodID}`, {}, config);
+                console.log(response.data); // response message
+            } catch (error) {
+                console.error('There was a problem with the PUT request:', error);
+            }
+        }
+
+    const handleButtonClick = async (id) => {
+        setProizvodID(id);
+        console.log(proizvodID)
+        await handleDodajKorpa(id);
+    }
 
     // useEffect(() => {
     //     axios.get()
@@ -32,29 +58,35 @@ const Proizvodi = ({ id }) => {
     //     .then((response) => setProizvodi(response.data))
     //     .catch((error) => console.log(error));
     // };
-
+    const role = vratiRole();
+    const klijent = role === "Klijent" ? "Klijent" : null
     return (
         <div>
             {/* <div style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "20px" }}>
                 <p style={{ fontSize: "40px" }}><strong><u>Proizvodi</u></strong></p>
             </div> */}
             <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "10px" }}>
-            {/* <div className="col-md-4 mb-3"> */}
-            {/* <Kategorije kategoije={kategorije} onSubmit={handleFilterSubmit} /> */}
-            {proizvodi.map((proizvod) => (
-                <Card >
-                    <div className="prozvod-card__image">
-                        <img src={proizvod.slika} alt="{salon.naziv}" />
-                    </div>
-                    <div>
-                        <h3>Naziv: {proizvod.naziv}</h3>
-                        <p>Cena: {proizvod.cena}</p>
-                        {console.log(proizvod.dostupnost)}
-                        <p>Dostupnost: {proizvod.dostupnost ? "NA STANJU" : "NEMA NA STANJU"}</p>
-                    </div>
-                </Card>
-            ))}
-        </div>
+                {/* <div className="col-md-4 mb-3"> */}
+                {/* <Kategorije kategoije={kategorije} onSubmit={handleFilterSubmit} /> */}
+                {proizvodi.map((proizvod) => (
+                    <Card >
+                        <div className="prozvod-card__image">
+                            <img src={proizvod.slika} alt="{salon.naziv}" />
+                        </div>
+                        <div>
+                            {klijent && <p>{proizvod.id}</p>}
+                            <h3>Naziv: {proizvod.naziv}</h3>
+                            <p>Cena: {proizvod.cena}</p>
+                            {console.log(proizvod.dostupnost)}
+                            <p>Dostupnost: {proizvod.dostupnost ? "NA STANJU" : "NEMA NA STANJU"}</p>
+                            <button className="btn-cart" onClick={() => handleButtonClick(proizvod.id)}>
+                                <BsCart className="btn-cart__icon" />
+                                Dodaj u korpu
+                            </button>
+                        </div>
+                    </Card>
+                ))}
+            </div>
         </div>
         // </div>
     );
--- Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonInfo.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonInfo.js
@@ -14,45 +14,45 @@ const SalonInfo = ({ salon, id }) => {
     };
     const role = vratiRole();
     const zahtev = role === "Klijent" ? "Zahtev" : null
-        return (
-            <div className="salon-items">
-                <div className="span-header">
-                    <span
-                        onClick={() => handleVrClick("Proizvodi")}
-                        className={selektovanaVr === "Proizvodi" ? "selected" : ""}
-                    >Proizvodi</span>{" "}
-                    <span
-                        onClick={() => handleVrClick("Usluge")}
-                        className={selektovanaVr === "Usluge" ? "selected" : ""}
-                    >Usluge</span>{" "}
-                    <span
-                        onClick={() => handleVrClick("Odgovori")}
-                        className={selektovanaVr === "Odgovori" ? "selected" : ""}
-                    >Pitanja</span>{" "}
-                    <span
-                        onClick={() => handleVrClick("Recenzije")}
-                        className={selektovanaVr === "Recenzije" ? "selected" : ""}
-                    >Recenzije</span>
-                    {zahtev && <span 
-                        onClick={() => handleVrClick("Zahtev")}
-                        className={selektovanaVr === "Zahtev" ? "selected" : ""}
-                    >Zahtev</span>}
-                </div>
-
-                <div className="items">
-                    {selektovanaVr === "Proizvodi" && <Proizvodi id={id} />}
-
-                    {selektovanaVr === "Usluge" && <Usluge id={id} />}
-
-                    {selektovanaVr === "Odgovori" && <Odgovori id={id} />}
-
-                    {selektovanaVr === "Recenzije" && <Recenzije id={id} />}
-
-                    {selektovanaVr === "Zahtev" }
-                </div>
-
+    return (
+        <div className="salon-items">
+            <div className="span-header">
+                <span
+                    onClick={() => handleVrClick("Proizvodi")}
+                    className={selektovanaVr === "Proizvodi" ? "selected" : ""}
+                >Proizvodi</span>{" "}
+                <span
+                    onClick={() => handleVrClick("Usluge")}
+                    className={selektovanaVr === "Usluge" ? "selected" : ""}
+                >Usluge</span>{" "}
+                <span
+                    onClick={() => handleVrClick("Odgovori")}
+                    className={selektovanaVr === "Odgovori" ? "selected" : ""}
+                >Pitanja</span>{" "}
+                <span
+                    onClick={() => handleVrClick("Recenzije")}
+                    className={selektovanaVr === "Recenzije" ? "selected" : ""}
+                >Recenzije</span>
+                {zahtev && <span
+                    onClick={() => handleVrClick("Zahtev")}
+                    className={selektovanaVr === "Zahtev" ? "selected" : ""}
+                >Zahtev</span>}
             </div>
-        );
+
+            <div className="items">
+                {selektovanaVr === "Proizvodi" && <Proizvodi id={id} />}
+
+                {selektovanaVr === "Usluge" && <Usluge id={id} />}
+
+                {selektovanaVr === "Odgovori" && <Odgovori id={id} />}
+
+                {selektovanaVr === "Recenzije" && <Recenzije id={id} />}
+
+                {selektovanaVr === "Zahtev"}
+            </div>
+
+        </div>
+    );
 };
 
 export default SalonInfo;
