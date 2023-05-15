271094c2ff3e9531c4df3aba453fce0a72e5213c
jovanas00 <jovanastojanovic394@gmail.com>
2023-05-15 17:29:11 +0200

    SalonPageIzmene

--- Aplikacija/Frontend/React/my-react-app/src/components/SalonPage/SalonPage.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/SalonPage/SalonPage.js
@@ -3,28 +3,24 @@ import { isSalon } from '../Auth/AuthSalon';
 import { Navigate } from 'react-router-dom';
 import { vratiRole } from '../Auth/VratiRole';
 import Header from '../Pocetna/Header';
+import SalonProfil from './SalonProfil';
 
 const SalonPage = () => {
-  if(isSalon())
-  {
-  return (
-    <Header>
-    <div>
-      <h2>Dobrodošli na stranicu Salona!</h2>
-      <p>Ovde možete pregledati informacije o salonu i njegove usluge.</p>
-      {/* Dodajte odgovarajući sadržaj za stranicu Salona */}
-    </div>
-    </Header>
-  );
+  if (isSalon()) {
+    return (
+          <div>
+            <Header />
+            <SalonProfil />
+          </div>
+    );
   }
-  else
-  {
+  else {
     const role = vratiRole();
-    if(role==="Klijent")
+    if (role === "Klijent")
       return <Navigate to={{ pathname: '/klijent' }} />
-    if(role==="Admin")
+    if (role === "Admin")
       return <Navigate to={{ pathname: '/admin' }} />
-    return <Navigate to={{ pathname: '/prijava'}}/>
+    return <Navigate to={{ pathname: '/prijava' }} />
   }
 };
 
