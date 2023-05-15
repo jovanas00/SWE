267e86cae26ccfd33a2ce88e4b43829f677df03e
jovanas00 <jovanastojanovic394@gmail.com>
2023-05-15 17:24:09 +0200

    SalonProfil

---
+++ Aplikacija/Frontend/React/my-react-app/src/components/SalonPage/SalonProfil.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { isSalon } from "../Auth/AuthSalon";
+import { Navigate } from 'react-router-dom';
+import { vratiRole } from '../Auth/VratiRole';
+import Header from '../Pocetna/Header';
+import Informacije from "./Informacije";
+
+const SalonProfil = () => {
+    if (isSalon()) {
+        return (
+            <div>
+                <Informacije />
+            </div>     
+        );
+    }
+    else {
+        const role = vratiRole();
+        if (role === "Admin")
+            return <Navigate to={{ pathname: '/admin' }} />
+        if (role === "Klijent")
+            return <Navigate to={{ pathname: '/klijent' }} />
+        return <Navigate to={{ pathname: '/prijava' }} />
+    }
+};
+
+export default SalonProfil;
