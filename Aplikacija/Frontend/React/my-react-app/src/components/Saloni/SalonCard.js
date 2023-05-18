5d0e56031b6994a54b401a3942a3f36c1b248795
AleksaT <95313323+Aleksa205@users.noreply.github.com>
2023-05-18 03:09:24 +0200

    Izmenjena stranica salona i prosek

--- Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonCard.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonCard.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Card from '../UI/Card';
-//import { Card } from 'react-bootstrap';
 import icon from '../../images/salonIcon.png';
 import './SalonCard.css';
 
 const SalonCard = ({ salon }) => {
+    const [prosecnaOcena, setProsecnaOcena] = useState(0);
+
+    useEffect(() => {
+        const dohvatiProsecnuOcenu = async () => {
+            try {
+                const id_salon=salon.id;
+                const response = await axios.get(`http://localhost:5169/Salon/ProsecnaOcena/${id_salon}`);
+                const data = response.data;
+                setProsecnaOcena(data.prosecnaOcena); // Ispravljeno ime polja
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        dohvatiProsecnuOcenu();
+    }, [salon.ID]);
+
     return (
         <Card className="salon-card">
             <div className="salon-card__image">
@@ -15,10 +32,11 @@ const SalonCard = ({ salon }) => {
                 <p>Adresa: {salon.adresa}</p>
                 <p>Grad: {salon.grad}</p>
                 <p>Broj telefona: {salon.brojTelefona}</p>
-                <p>Prosecna ocena: {salon.prosecnaOcena}</p>
+                <p>Prosečna ocena: {prosecnaOcena}</p>
             </div>
         </Card>
     );
 };
 
 export default SalonCard;
+
--- Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonStranica.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Saloni/SalonStranica.js
@@ -2,36 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../Pocetna/Header';
-import SalonCard from './SalonCard';
 import Card from '../UI/Card';
 import SalonInfo from './SalonInfo';
 import "./SalonStranica.css";
 import Korpa from './Korpa';
 import Cookies from 'js-cookie';
+import icon from '../../images/salonIcon.png';
 import { isKlijent } from '../Auth/AuthKlijent';
 import { vratiRole } from '../Auth/VratiRole';
 
 const SalonStranica = () => {
-    const { id } = useParams(); // uzima se id iz Route(prosledjuje se u App.js)
-    const [salon, setSalon] = useState(null); // inicijalizuje se state salon na null
+    const { id } = useParams();
+    const [salon, setSalon] = useState(null);
+    const [prosecnaOcena, setProsecnaOcena] = useState(0);
     const role = vratiRole();
     const klijent = role === "Klijent" ? "Klijent" : null;
-    
+
     useEffect(() => {
-        // uzima se salon iz baze
         if (id) {
             axios.get(`http://localhost:5169/Salon/VratiSalon/${id}`)
                 .then(response => {
                     console.log(response.data);
-                    setSalon(response.data); // setujemo novi state salon na dobijene podatke
+                    setSalon(response.data);
                 })
                 .catch(error => {
                     console.error(error);
                 });
-
-            }
-        if(klijent)
-        {
+        }
+        if (klijent) {
             axios.delete(`http://localhost:5169/Klijent/IsprazniKorpu/${id}`, {
                 headers: {
                     'Authorization': `Bearer ${Cookies.get('token')}`
@@ -44,36 +42,48 @@ const SalonStranica = () => {
                     console.error(error);
                 });
         }
-    }, [id]); // ova se funkcija zove kad se promeni id
+        const dohvatiProsecnuOcenu = async () => {
+            try {
+                const id_salon = id;
+                const response = await axios.get(`http://localhost:5169/Salon/ProsecnaOcena/${id_salon}`);
+                const data = response.data;
+                setProsecnaOcena(data.prosecnaOcena);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        dohvatiProsecnuOcenu();
+    }, [id]);
 
     if (!salon) {
-        return <div></div>; // iz nekog razloga kad ova rpovera ne postoji greska se javi 
-    }//ovde se samo treba pojavi ova poruka dok se ne uzmu podaci iz bazez
+        return <div></div>;
+    }
 
     return (
         <div>
             <Header />
-            <div className="salon-info">
-                <div className="salon-card__div">
-                    <SalonCard salon={salon} />
+
+                <div className="salon-info1">
+                    <div className="salon-card1">
+                        <div className="salon-card__image1">
+                            <img className="slika" src={icon} alt="{salon.naziv}" />
+                        </div>
+                        <div className="salon-card__text1">
+                            <h5>Naziv: {salon.naziv}</h5>
+                            <p>Adresa: {salon.adresa}</p>
+                            <p>Grad: {salon.grad}</p>
+                            <p>Broj telefona: {salon.brojTelefona}</p>
+                            <p>Prosecna ocena: {prosecnaOcena}</p>
+                        </div>
+                    </div>
+                    <div className="salon-info__items">
+                        <SalonInfo salon={salon} id={id} />
+                    </div>
                 </div>
-                {/* ako se ne koristi SalonCard */}
-                {/* <div className="salon-card__image">
-                    <img src={icon} alt="{salon.naziv}" />
-                </div> */}
-                {/* <div className="salon-card__text">
-                    <h5>Naziv: {salon.naziv}</h5>
-                    <p>Adresa: {salon.adresa}</p>
-                    <p>Grad: {salon.grad}</p>
-                    <p>Broj telefona: {salon.brojTelefona}</p>
-                    <p>Prosecna ocena: {salon.prosecnaOcena}</p>
-                </div> */}
-                <div className="salon-info__items">
-                    <SalonInfo salon={salon} id={id} />
-                </div>
-            </div>
         </div>
     );
 }
 
 export default SalonStranica;
+
