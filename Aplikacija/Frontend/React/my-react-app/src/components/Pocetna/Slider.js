62b41eeaad260ad1d24e0d49a914dac951c84cde
AleksaT <95313323+Aleksa205@users.noreply.github.com>
2023-05-23 03:26:10 +0200

    Responsive teksta na slajdovima

--- Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Slider.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Slider.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import slider1 from '../../images/slider/slider1.jpeg';
 import slider2 from '../../images/slider/slider2.jpg';
 import slider3 from '../../images/slider/slider3.jpg';
 import Carousel from 'react-bootstrap/Carousel';
 
 const Slider = ({ slides }) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+  
+    const fontSize = windowWidth < 768 ? "17px" : "26px";
     return (
         <div className="carousel">
             <Carousel>
@@ -17,7 +32,7 @@ const Slider = ({ slides }) => {
                     />
                     <Carousel.Caption>
                         <div style={{ color: "#ff1a1a" }}>
-                            <h3>Vaši ljubimci zaslužuju najbolje, a mi im to pružamo!</h3>
+                            <h3 style={{ fontSize }}>Vaši ljubimci zaslužuju najbolje, a mi im to pružamo!</h3>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -31,7 +46,7 @@ const Slider = ({ slides }) => {
 
                     <Carousel.Caption>
                         <div style={{ color: "#ff1a1a" }}>
-                            <h3>Obezbedite svojim ljubimcima pažnju i negu koju zaslužuju u našim salonima za kućne ljubimce.</h3>
+                            <h3 style={{ fontSize }}>Obezbedite svojim ljubimcima pažnju i negu koju zaslužuju u našim salonima za kućne ljubimce.</h3>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -45,7 +60,7 @@ const Slider = ({ slides }) => {
 
                     <Carousel.Caption>
                         <div style={{ color: "#ff1a1a" }}>
-                            <h3>U našim salonima za kućne ljubimce možete pronaći širok izbor usluga i proizvoda za negu vaših ljubimaca.</h3>
+                            <h3 style={{ fontSize }}>U našim salonima za kućne ljubimce možete pronaći širok izbor usluga i proizvoda za negu vaših ljubimaca.</h3>
                         </div>
 
                     </Carousel.Caption>
