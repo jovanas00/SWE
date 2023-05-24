23bbbf60459c6c6b7ac9611e1856bde1ed91368d
unknown <danilonikolic@elfak.rs>
2023-05-24 21:18:19 +0200

    Pocetna footer

--- Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Footer.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Footer.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Footer.css';
+import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <div >
-            <hr />
+            <div>
+                <hr />
+                <div className="footer-icons">
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram className="footer-icon" />
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FaFacebookF className="footer-icon" />
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter className="footer-icon" />
+                    </a>
+                </div>
                 <p className="footer-copyright">
-                   @{new Date().getFullYear()} AJD Team. 
+                    @{new Date().getFullYear()} AJD Team.
                 </p>
             </div>
         </div>
