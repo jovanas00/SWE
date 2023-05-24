9abab5653cc2b2b0654e262bcc86ba1f04952073
unknown <danilonikolic@elfak.rs>
2023-05-24 21:10:04 +0200

    Pocetna izgled

--- Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Slider.js
+++ Aplikacija/Frontend/React/my-react-app/src/components/Pocetna/Slider.js
@@ -3,22 +3,23 @@ import slider1 from '../../images/slider/slider1.jpeg';
 import slider2 from '../../images/slider/slider2.jpg';
 import slider3 from '../../images/slider/slider3.jpg';
 import Carousel from 'react-bootstrap/Carousel';
+import './Slider.css'
 
 const Slider = ({ slides }) => {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
-      const handleResize = () => {
-        setWindowWidth(window.innerWidth);
-      };
-  
-      window.addEventListener("resize", handleResize);
-  
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
     }, []);
-  
+
     const fontSize = windowWidth < 768 ? "17px" : "26px";
     return (
         <div className="carousel">
@@ -26,11 +27,11 @@ const Slider = ({ slides }) => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        style={{maxWidth: "100%", height: "auto"}}
+                        style={{ maxWidth: "100%", height: "auto" }}
                         src={slider1}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-caption">
                         <div style={{ color: "#ff1a1a" }}>
                             <h3 style={{ fontSize }}>Vaši ljubimci zaslužuju najbolje, a mi im to pružamo!</h3>
                         </div>
@@ -39,12 +40,12 @@ const Slider = ({ slides }) => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        style={{maxWidth: "100%", height: "auto"}}
+                        style={{ maxWidth: "100%", height: "auto" }}
                         src={slider2}
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-caption">
                         <div style={{ color: "#ff1a1a" }}>
                             <h3 style={{ fontSize }}>Obezbedite svojim ljubimcima pažnju i negu koju zaslužuju u našim salonima za kućne ljubimce.</h3>
                         </div>
@@ -53,16 +54,15 @@ const Slider = ({ slides }) => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        style={{maxWidth: "100%", height: "auto"}}
+                        style={{ maxWidth: "100%", height: "auto" }}
                         src={slider3}
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-caption">
                         <div style={{ color: "#ff1a1a" }}>
                             <h3 style={{ fontSize }}>U našim salonima za kućne ljubimce možete pronaći širok izbor usluga i proizvoda za negu vaših ljubimaca.</h3>
                         </div>
-
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
